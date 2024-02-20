34a95a9441e35f0fd49f56b3a1f6d9cd8a41cbad
Brandon C <18476133+raisinbran@users.noreply.github.com>
2024-02-19 22:23:06 -0800

    240219

---
+++ js/auxiliary.js
@@ -0,0 +1,50 @@
+// Add custom JS here
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+$(function(){
+    $("[data-toggle=popover]").popover({
+        html : true,
+        content: function() {
+          var content = $(this).attr("data-popover-content");
+          return $(content).children(".popover-body").html();
+        },
+        title: function() {
+          var title = $(this).attr("data-popover-content");
+          return $(title).children(".popover-heading").html();
+        }
+    });
+});
+
+$('.popover-dismiss').popover({
+  trigger: 'focus'
+});
+
+$(function() {
+    $("#footer").load("footer.html");
+});
+
+function updateTimer() {
+  future  = Date.parse("December 31, 2024 12:00:00");
+  now     = new Date();
+  diff    = future - now;
+
+  days  = Math.floor( diff / (1000*60*60*24) );
+  hours = Math.floor( diff / (1000*60*60) );
+  mins  = Math.floor( diff / (1000*60) );
+  secs  = Math.floor( diff / 1000 );
+
+  d = days;
+  h = hours - days  * 24;
+  m = mins  - hours * 60;
+  s = secs  - mins  * 60;
+
+  document.getElementById("timer")
+    .innerHTML =
+      '<div>' + d + '<span>days</span></div>' +
+      '<div>' + h + '<span>hours</span></div>' +
+      '<div>' + m + '<span>minutes</span></div>' +
+      '<div>' + s + '<span>seconds</span></div>' ;
+}
+setInterval('updateTimer()', 1000 );
